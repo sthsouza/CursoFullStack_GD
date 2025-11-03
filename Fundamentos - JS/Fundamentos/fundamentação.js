7ffd115f6d82ae542ae906ad36cd136d5018224a
sthsouza <astheffanysouza@gmail.com>
2025-11-03 08:30:40 -0300

    Conceitos da fundamentação em JS

---
+++ Fundamentos - JS/Fundamentos/fundamentação.js
@@ -0,0 +1,28 @@
+/*
+
+JAVA SCRIPT
+-- Criada por Brendan Eich
+-- Em 1995
+
+-- Serve para o dinamismo de uma página web
+-- Usa engine V8
+    Foi apartir do engine v9 que surgiu o node.js, que permite explorar vcodigos  tanto pelo lado do servidor e do cliente.
+
+--ECMA SCRIPT
+    Ia se chamar Java Script, mas a oracle já havia patentiado esse nme. Então, como estavam associados ao ECMA, apropiaram-se do nome. Mas os programadores ainda usam o nome JS.
+
+-- Versões
+    As versões do JS são chamadas de ES1, es2, es3...es6
+    Apartir de 2016, as versões foram nomeadas de ES(ano): ES2016, ES2017...ES2021 (Ultima versão)
+
+--ES.Next
+    ES.Next é um nome dinâmico que se refere a qualquer que seja a próxima versão no momento da escrita.
+
+--HELLO WORLD
+    No js:
+    console.log ("Hello World")
+
+    No html:
+    <script src:"arquivo.js"></script>
+
+*/
