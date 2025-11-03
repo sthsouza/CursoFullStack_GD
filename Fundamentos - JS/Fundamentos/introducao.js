20af9a38decc8c928f0a6663a9006436487df1a6
sthsouza <astheffanysouza@gmail.com>
2025-11-03 08:07:52 -0300

    Adiciona Atividade de JS

--- Fundamentos - JS/Fundamentos/introducao.js
+++ Fundamentos - JS/Fundamentos/introducao.js
@@ -3,4 +3,13 @@
 é chamado de ECMAScript
 -- Linguagem de alto nivel
 --É interpretada (tradução acontece durante a execção do problema) 
+
+ABORDAGENS DE JS
+-- Interno
+    Dentro o próprio Html, através da tag script
+
+-- Externo
+    O código js está externo ao arquivo html, no html referencia-se o js atraves do href
+
+--Recomenda-se usar externo paa elhor gerenciamento.
 */
---
+++ Fundamentos - JS/Fundamentos/meu1js.js
@@ -0,0 +1,1 @@
+console.log ("Olá, meu nome é Stheffany!")
