de6b767aeff96d103c861e69ccf62a9548e9dfbf
sthsouza <astheffanysouza@gmail.com>
2025-11-06 16:40:09 -0300

    Conceitos de entradas e saídas de dados em JS

---
+++ Fundamentos - JS/Variáveis/avariaveis.js
@@ -0,0 +1,5 @@
+let alturaStheffany = 1.58
+let idadeStheffany = 15
+let nomeStheffany = 'Stheffany'
+let carteiraTem = false
+let cidadeStheffany = 'Sapiranga'
---
+++ Fundamentos - JS/Variáveis/entradasesaidas.js
@@ -0,0 +1,19 @@
+// Entradas e saidas de dados
+// -------prompt e confirm
+//  O prompt serve para pedir informações ao usuário
+// O confirm faz perguntas, também, mas com sim ou não (valores lógicos)
+    
+// -------alert e console  
+    // São informações que o programa oferece ao usuário
+    // o alert mostra a informação em uma caixa de diálogo
+
+let nome = prompt('Olá, qual é o seu nome?')
+console.log (nome)
+
+let permissaoMicrofone = confirm('Autorizar o acesso ao seu microfone?')
+console.log (permissaoMicrofone)
+
+alert('Obrigada, recebemos suas informações! Seja bem-vindo!')
+
+let idade = prompt('Qual a sua idade?')
+console.log (idade)
