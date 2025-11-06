6e5389c9c44e224cdbccf2766d60422669cfc0e3
sthsouza <astheffanysouza@gmail.com>
2025-11-06 16:00:44 -0300

    Atividades com variáveis

---
+++ Fundamentos - JS/Variáveis/variavel.js
@@ -0,0 +1,15 @@
+// Existem 3 tipos de palavras usadas para declarar uma variável:
+
+// Var e let   
+//     Podem ser modificada sao longo do código, ao contrário de Const.
+// --Recomenda-se o uso de let, pois varé de uma versão aontiga o js
+
+var nome = 'Stheffany'
+let idade = 15
+const cidade = 'Salvador'
+
+console.log (nome, cidade, idade)
+
+idade = 5
+
+console.log (idade)
