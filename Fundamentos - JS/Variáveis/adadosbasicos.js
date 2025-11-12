b6516888695775cb941d214d807b8b224972dc9d
sthsouza <astheffanysouza@gmail.com>
2025-11-12 14:40:00 -0300

    Conceitos de tipos  primitivos e dados básicos no JS

---
+++ Fundamentos - JS/Variáveis/adadosbasicos.js
@@ -0,0 +1,9 @@
+console.log('---------- VARIÁVEL CHECKER -------------')
+let let1 = null
+let let2 = undefined
+let let3 = prompt ('Insira aqui o dado que você quer verificar:' )
+
+console.log ('A primeira variável é: ' + typeof let1)
+console.log ('A segunda variável é: ' + typeof let2)
+console.log ('A terceira variável é: ' + typeof let3)
+
---
+++ Fundamentos - JS/Variáveis/atiposprimitivos.js
@@ -0,0 +1,11 @@
+console.log ("Questão 1 -----------------")
+const num = 57
+console.log ('A variável numm é: ' + num)
+
+console.log ("Questão 2 -----------------")
+const estaChovendo = false
+console.log ("Está chovendo lá fora?" + typeof estaChovendo)
+
+console.log ("Questão 3 -----------------")
+const nomeCompleto = 'Stheffany Souza'
+console.log (`O nome completo é ${nomeCompleto}`)
