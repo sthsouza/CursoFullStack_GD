83a3ebd21a490a4a6d1acae765965cb438bc947b
sthsouza <astheffanysouza@gmail.com>
2025-11-14 16:26:35 -0300

    Adiciona conceitos e atividades de listas Arrey

---
+++ Fundamentos - JS/Variáveis/listaarray.js
@@ -0,0 +1,30 @@
+//estrutura de dados que permite armazenar várias informações em uma única variável
+
+const frutas = ['Maçã', 'lARANJA', 12]
+console.log (frutas)
+//a contagem os itens começa em 0
+
+//para obter o primeiro item na lista e frutas, acessamos o indice 0
+console.log(frutas[0])
+
+//para saber o tamanho de um array, usamos a propriedade lenght
+console.log (frutas.length)
+
+//Para operar o adiconar informaçoes no array:
+frutas [1] = 'Morango'
+
+//para aicionar mais
+frutas[frutas.length]= 'Abacate'
+console.log (frutas)
+
+//Atividade
+console.log ('-------CORES------')
+const cores = ['Vermelho', 'Laranja', 'Amarelo', 'Verde', 'Azul', 'Anil', 'Violeta']
+console.log('Cores da Lista:')
+console.log (cores)
+console.log('----------')
+console.log('Terceira cor da lista:')
+console.log (cores[2])
+
+
+
