b7158c75fe7d9c0993843a04831cba6e487942df
sthsouza <astheffanysouza@gmail.com>
2025-11-18 17:36:26 -0300

    Adiciona concitos e atividades de operadores Aritiméticos

---
+++ Fundamentos - JS/Operadores/operadores.js
@@ -0,0 +1,6 @@
+//o que são??
+//simbolos usados para realizar operações netre valores
+//aritiméticos: calculos matemáticos
+//atribuição: para definir e atualizar valores
+//relacionais: para comparar valores
+//logicos: true ou false 
---
+++ Fundamentos - JS/Operadores/operadoresaritimeticos.js
@@ -0,0 +1,27 @@
+const num1 = 10
+const num2 = 5
+
+console.log (`Num1 = 10, num2 = 5`)
+console.log (`//////////////`)
+console.log (`Adição: ${num1+num2}`)
+console.log (`Subtração: ${num1-num2}`)
+console.log (`Multiplicação: ${num1*num2}`)
+console.log (`Divisão: ${num1/num2}`)
+console.log (`Resto da divisão: ${num1%num2}`)
+console.log (`Exponenciação: ${num1+num2}`)
+
+let decremento = 5
+decremento--
+console.log(decremento)
+
+let incremento = 5
+incremento++
+console.log(incremento)
+
+console.log('ativdade/////////')
+console.log ('Questão 1')
+console.log (`O funcionário trabalha ${8*5} horas na semana`)
+
+console.log('ativdade/////////')
+console.log ('Questão 2')
+console.log (`O clinte pagou: ${(50*3)-10}`)
