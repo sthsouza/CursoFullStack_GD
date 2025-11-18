459561999666c62eff80cd4d81cb62d0e759a976
sthsouza <astheffanysouza@gmail.com>
2025-11-18 17:06:38 -0300

    Estruturas complexas

---
+++ Fundamentos - JS/Dados Estruturados/estruturas-complexas.js
@@ -0,0 +1,29 @@
+const produtos = [
+    {
+        nome: 'Borracha',
+        valor: 3.99,
+        categoria: 'Material escolar'
+    },
+
+    {
+        nome: 'Lápis',
+        valor: 1.99,
+        categoria: 'Material escolar'
+    },
+
+    {
+        nome: 'Copo',
+        valor: 8,
+        categoria: 'Material domiciliar'
+    },
+
+    {
+        nome: 'Caneta',
+        valor: 5.99,
+        categoria: 'Material escolar'
+    }
+]
+
+console.log ('Nome do produto: ', produtos[2].nome)
+console.log ('Preço: ', produtos[2].valor)
+console.log ('Categoria do produto: ', produtos[2].categoria)
