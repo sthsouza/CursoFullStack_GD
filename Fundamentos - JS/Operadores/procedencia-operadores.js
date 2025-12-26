a4e7d870179890f3c96bcca75d372d46e77998bb
sthsouza <astheffanysouza@gmail.com>
2025-12-26 14:43:42 -0300

    Adiciona conceitos e atividades sobre procedência de operadores

---
+++ Fundamentos - JS/Operadores/procedencia-operadores.js
@@ -0,0 +1,18 @@
+console.log (1+1+4)
+console.log ((2+1)*4)
+console.log (5 > 3 && 4 < 2 || 1 == 1)
+
+console.log ("ATIVIDADE 1 ------------")
+const precoProduto = 100
+const descontoValor = (precoProduto/100)*20
+const valorEntrega = 5
+const precoFinal = (precoProduto-descontoValor)+ valorEntrega
+console.log (`O preço final do seu produto é: ${precoFinal}`)
+
+console.log ("ATIVIDADE 2 ------------")
+const numProvas = 3
+const provaNum1 = 8
+const provaNum2 = 4
+const provaNum3 = 10
+const mediaProva = (provaNum1 + provaNum2 + provaNum3)/numProvas
+console.log (`A média das suas notas é: ${mediaProva}`)
