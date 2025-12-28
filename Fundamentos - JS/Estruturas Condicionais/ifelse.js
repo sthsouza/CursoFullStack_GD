43ce6a47971d647c7370a7ec07e571243de3e0ce
sthsouza <astheffanysouza@gmail.com>
2025-12-28 13:01:03 -0300

    Atividades sobre IF ELSE

--- Fundamentos - JS/Estruturas Condicionais/ifelse.js
+++ Fundamentos - JS/Estruturas Condicionais/ifelse.js
@@ -46,3 +46,27 @@ if (estaEnsolarado) {
 const resultado = estaEnsolarado ? 'Não levar guarda chuva' : 'levar guarda chuva'
 
 console.log (resultado)
+
+console.log ("ATIVIDADE 1 ---------")
+
+const nomePessoa = prompt("Olá, insira seu nome: ")
+const idadePessoa = prompt ("Insira sua idade: ")
+const numeroCompras = prompt ("Insira o númro de compras que fizeste até agora (Apenas números): ")
+const membroVip = prompt ('É membro VIP (Coloque N para NÃo e S para SIM): ')
+
+console.log (`Seu nome: ${nomePessoa}`)
+console.log (`Sua idade: ${idadePessoa}`)
+console.log (`Número de compras: ${numeroCompras}`)
+console.log (`É membro VIP: ${membroVip}`)
+
+const resultadoPremio = (idadePessoa >= 18 && numeroCompras >= 5) ||membroVip == 'S' ? 'Eba, você pode retirar o prêmio!' : 'Ooops, você não poded retirar o prêmio!'
+
+console.log (resultadoPremio)
+
+console.log ("ATIVIDADE 2 ---------")
+const idadeMotorista =  prompt ('Sua idade (apenas números): ')
+const carteiraHabilitcao = prompt ('Possui carteira de habilitação? S para SIM e N para NÃO: ')
+
+const podeDirigir = idadeMotorista >= 18 && carteiraHabilitcao == 'S' ? 'Você pode dirigir.' : 'Você não pode dirigir.'
+
+console.log (podeDirigir)
