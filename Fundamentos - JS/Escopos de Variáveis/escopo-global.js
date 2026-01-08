93b7d10b959df8018ed9d9ed874ea96784f9ad1e
sthsouza <astheffanysouza@gmail.com>
2026-01-07 23:26:56 -0300

    Adiciona atividades de ESCOPO GLOBAL

---
+++ Fundamentos - JS/Escopos de Variáveis/escopo-global.js
@@ -0,0 +1,14 @@
+console.log ('ATIVIDADE 1 ------')
+
+let contadorGlobal = 0
+contadorGlobal++
+console.log (contadorGlobal)
+
+console.log ('ATIVIDADE 2 ------')
+const mensagemGlobal = 'Olá, mundo!'
+console.log (mensagemGlobal)
+
+mensagemGlobal = 'Mudança de mensagem!'
+console.log (mensagemGlobal)
+
+//erro porque a variável foi defnida como const, não podendo ter seu valor alterado
