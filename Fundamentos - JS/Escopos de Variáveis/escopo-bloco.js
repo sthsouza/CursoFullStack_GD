1618529b99651324e438971b25c3d077c82c5e0c
sthsouza <astheffanysouza@gmail.com>
2026-01-08 00:12:10 -0300

    Adiciona atividades de ESCOPO DE BLOCO

---
+++ Fundamentos - JS/Escopos de Variáveis/escopo-bloco.js
@@ -0,0 +1,17 @@
+// console.log ('ATIVIDADE 1 --------')
+
+// if (true) {
+//     let valorBloco = 50
+//     console.log (valorBloco)
+// }
+
+// console.log (valorBloco)
+
+console.log ('ATIVIDADE 2 --------')
+const numero = 10 
+for (let i = 0; i <1; i++) {
+    let resultado = 3 * numero
+    console.log (resultado)
+}
+ console.log (resultado)
+
