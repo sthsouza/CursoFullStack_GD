9b8a5552e96e469734bdcae968f22ae63b997cfe
sthsouza <astheffanysouza@gmail.com>
2026-01-08 08:35:21 -0300

    Adiciona conceitos e atividades sobre declarações de funções

---
+++ Fundamentos - JS/Funções/declaração-execução-funções.js
@@ -0,0 +1,12 @@
+function saudacao () {
+    console.log ('Olá, bem-vindo! ')
+}
+
+saudacao ()
+
+console.log ('ATIVIDADE 1 -------')
+function exibirMensagem() {
+    console.log ('Esse é o meu primeiro exercicio com funções!')
+}
+
+exibirMensagem()
