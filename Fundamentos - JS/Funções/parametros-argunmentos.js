43fd82ade7678ff9bab4aa3c1d438b8185c85e99
sthsouza <astheffanysouza@gmail.com>
2026-01-08 08:44:24 -0300

    Adiciona conceitos e atividades sobre parametros e argumentos

---
+++ Fundamentos - JS/Funções/parametros-argunmentos.js
@@ -0,0 +1,15 @@
+function saudacaoPersonalizada (nome) {
+    console.log (`Olá, ${nome}`)
+}
+
+saudacaoPersonalizada ('Maria')
+
+//Nesse exemplo, "nome" é um parametro da função "saudacaoPersonalizada"
+//O parametro permite que passemos um valor externo para a função. Quando chamamos a função, passamos um argumento
+
+console.log ('ATIVIDADE ---------')
+function calcularTriplo(numero) {
+    console.log (numero*3)
+}
+
+calcularTriplo(3)
